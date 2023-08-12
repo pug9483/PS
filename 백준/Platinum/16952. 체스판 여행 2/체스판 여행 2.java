8dbdfc8030a0db4ce270b49c310f1f270f41f572
pug <141905308+kiki9484@users.noreply.github.com>
2023-08-13 02:43:25 +0900

    [Platinum IV] Title: 체스판 여행 2, Time: 340 ms, Memory: 22868 KB -BaekjoonHub

--- 백준/Platinum/16952. 체스판 여행 2/체스판 여행 2.java
+++ 백준/Platinum/16952. 체스판 여행 2/체스판 여행 2.java
@@ -10,15 +10,14 @@ public class Main {
         int type;
         int dist;
         int level;
-        int swtch;
-
-        public Point(int y, int x, int type, int dist, int level, int swtch) {
+        int change;
+        public Point(int y, int x, int type, int dist, int level, int change) {
             this.y = y;
             this.x = x;
             this.type = type;
             this.dist = dist;
             this.level = level;
-            this.swtch = swtch;
+            this.change = change;
         }
     }
 
@@ -30,8 +29,8 @@ public class Main {
             {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}},
             {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}},
             {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
+    public static int retSwitch = INF;
     public static int retMove = INF;
-    public static int retSwtch = INF;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -45,54 +44,61 @@ public class Main {
         }
         solve();
         System.out.println(retMove);
-        System.out.println(retSwtch);
+        System.out.println(retSwitch);
     }
 
     private static void solve() {
-        Queue<Point> q = new LinkedList<>();
-        boolean[][][][][] visited = new boolean[N][N][3][N * N + 1][4 * N * N + 1];
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingInt((Point a) -> a.dist).thenComparingInt(a -> a.change));
+        int[][][][] dist = new int[N][N][3][N * N + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < 3; k++) {
+                    Arrays.fill(dist[i][j][k], INF);
+                }
+            }
+        }
         out:
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (board[i][j] == 1) {
                     for (int type = 0; type < 3; type++) {
-                        q.add(new Point(i, j, type, 0, 1, 0));
-                        visited[i][j][type][1][0] = true;
+                        pq.add(new Point(i, j, type, 0, 1, 0));
+                        dist[i][j][type][1] = 0;
                     }
                     break out;
                 }
             }
         }
-        while (!q.isEmpty()) {
-            Point here = q.poll();
-            if (here.level == N * N && here.dist <= retMove) {
-                retMove = Math.min(retMove, here.dist);
-                retSwtch = Math.min(retSwtch, here.swtch);
+        while (!pq.isEmpty()) {
+            Point here = pq.poll();
+            if (here.level == N * N) {
+                retSwitch = here.change;
+                retMove = here.dist;
+                return;
             }
 
             for (int type = 0; type < 3; type++) {
-                if (here.swtch >= 4 * N * N - 1) continue;
-                if (here.type == type || visited[here.y][here.x][type][here.level][here.swtch + 1]) continue;
-                q.add(new Point(here.y, here.x, type, here.dist + 1, here.level, here.swtch + 1));
-                visited[here.y][here.x][type][here.level][here.swtch + 1] = true;
+                if (here.type == type || dist[here.y][here.x][type][here.level] <= here.dist + 1) continue;
+                pq.add(new Point(here.y, here.x, type, here.dist + 1, here.level, here.change+1));
             }
-            if (here.type == 0) goKnight(here, visited, q);
-            else goBishopOrRook(here, visited, q);
+            if (here.type == 0) goKnight(here, dist, pq);
+            else goBishopOrRook(here, dist, pq);
         }
     }
 
-    private static void goBishopOrRook(Point here, boolean[][][][][] visited, Queue<Point> q) {
+    private static void goBishopOrRook(Point here, int[][][][] dist, PriorityQueue<Point> pq) {
         for (int d = 0; d < dir[here.type].length; d++) {
             int ny = here.y + dir[here.type][d][0];
             int nx = here.x + dir[here.type][d][1];
 
             while (check(ny, nx)) {
-                if (board[ny][nx] == here.level + 1 && !visited[ny][nx][here.type][here.level + 1][here.swtch]) {
-                    visited[ny][nx][here.type][here.level + 1][here.swtch] = true;
-                    q.offer(new Point(ny, nx, here.type, here.dist + 1, here.level + 1, here.swtch));
-                } else if (!visited[ny][nx][here.type][here.level][here.swtch]) {
-                    visited[ny][nx][here.type][here.level][here.swtch] = true;
-                    q.offer(new Point(ny, nx, here.type, here.dist + 1, here.level, here.swtch));
+                if (board[ny][nx] == here.level + 1 && dist[ny][nx][here.type][here.level + 1] > here.dist + 1) {
+                    dist[ny][nx][here.type][here.level + 1] = here.dist + 1;
+                    pq.offer(new Point(ny, nx, here.type, here.dist + 1, here.level + 1, here.change));
+                }
+                else if (dist[ny][nx][here.type][here.level] > here.dist+1) {
+                    dist[ny][nx][here.type][here.level] = here.dist + 1;
+                    pq.offer(new Point(ny, nx, here.type, here.dist + 1, here.level, here.change));
                 }
                 ny += dir[here.type][d][0];
                 nx += dir[here.type][d][1];
@@ -100,17 +106,17 @@ public class Main {
         }
     }
 
-    private static void goKnight(Point here, boolean[][][][][] visited, Queue<Point> q) {
+    private static void goKnight(Point here, int[][][][] dist, PriorityQueue<Point> pq) {
         for (int d = 0; d < dir[here.type].length; d++) {
             int ny = here.y + dir[here.type][d][0];
             int nx = here.x + dir[here.type][d][1];
-            if (!check(ny, nx) || visited[ny][nx][here.type][here.level][here.swtch]) continue;
-            if (board[ny][nx] == here.level + 1 && !visited[ny][nx][here.type][here.level + 1][here.swtch]) {
-                visited[ny][nx][here.type][here.level + 1][here.swtch] = true;
-                q.offer(new Point(ny, nx, here.type, here.dist + 1, here.level + 1, here.swtch));
-            } else if (!visited[ny][nx][here.type][here.level][here.swtch]) {
-                visited[ny][nx][here.type][here.level][here.swtch] = true;
-                q.offer(new Point(ny, nx, here.type, here.dist + 1, here.level, here.swtch));
+            if (!check(ny, nx) || dist[ny][nx][here.type][here.level] <= here.dist + 1) continue;
+            if (board[ny][nx] == here.level + 1 && dist[ny][nx][here.type][here.level + 1] > here.dist + 1) {
+                dist[ny][nx][here.type][here.level + 1] = here.dist + 1;
+                pq.offer(new Point(ny, nx, here.type, here.dist + 1, here.level + 1, here.change));
+            } else if (dist[ny][nx][here.type][here.level] > here.dist + 1) {
+                dist[ny][nx][here.type][here.level] = here.dist + 1;
+                pq.offer(new Point(ny, nx, here.type, here.dist + 1, here.level, here.change));
             }
         }
     }
