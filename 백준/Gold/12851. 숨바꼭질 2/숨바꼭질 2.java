b639a3758ad103acfc109de623319d45d2889b99
pug <141905308+kiki9484@users.noreply.github.com>
2023-08-12 23:43:24 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 172 ms, Memory: 25648 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static int N, K;
+    public static int[] dist;
+    public static boolean check(int num){
+        return num >= 0 && num <= 100000;
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dist = new int[100001];
+        Arrays.fill(dist, -1);
+        bfs();
+        System.out.println(dist[K]);
+        System.out.println(reconstruct(K));
+    }
+
+    private static int reconstruct(int here) {
+        if(here < 0 || here > 100000) return 0;
+        if(here == N) return 1;
+        int ret = 0;
+        if(here - 1 >= 0 && dist[here-1] + 1 == dist[here]) ret += reconstruct(here-1);
+        if(here + 1 <= 100000 && dist[here+1] + 1 == dist[here]) ret += reconstruct(here+1);
+        if(here > 0 && here % 2 == 0 && dist[here / 2] + 1 == dist[here]) ret += reconstruct(here / 2);
+        return ret;
+    }
+
+    private static void bfs() {
+        dist[N] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(N);
+        while (!q.isEmpty()) {
+            int here = q.poll();
+            int next = here + 1;
+            if(check(next) && dist[next] == -1){
+                q.add(next);
+                dist[next] = dist[here] + 1;
+            }
+            next = here - 1;
+            if(check(next) && dist[next] == -1){
+                q.add(next);
+                dist[next] = dist[here] + 1;
+            }
+            next = here * 2;
+            if(check(next) && dist[next] == -1){
+                q.add(next);
+                dist[next] = dist[here] + 1;
+            }
+        }
+    }
+}
