a0c3004dcdb530171204ca6c08f716de52d1b8d7
pug <141905308+kiki9484@users.noreply.github.com>
2023-09-06 17:07:15 +0900

    [Platinum V] Title: 고층 빌딩, Time: 176 ms, Memory: 24184 KB -BaekjoonHub

--- 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
@@ -7,7 +7,6 @@ public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final int MOD = 1_000_000_007;
     private static long[][][] cache = new long[101][101][101];
-    //cache[n][l][r] : n개의 건물 중 왼쪽에서 보이는 건물이 l개, 오른쪽에서 보이는 건물이 r개
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -15,9 +14,8 @@ public class Main {
         int R = Integer.parseInt(st.nextToken());
 
         cache[1][1][1] = 1;
-        cache[2][2][1] = cache[2][1][2] = 1;
 
-        for (int n = 3; n <= N; n++) {
+        for (int n = 2; n <= N; n++) {
             for (int l = 1; l <= L; l++) {
                 for (int r = 1; r <= R; r++) {
                     cache[n][l][r] = (cache[n][l][r] + cache[n-1][l-1][r]) % MOD;
@@ -28,6 +26,4 @@ public class Main {
         }
         System.out.println(cache[N][L][R]);
     }
-
-
 }
