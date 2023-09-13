0268330df12920cb647736ec0af24fd6d6eef5a7
pug <141905308+kiki9484@users.noreply.github.com>
2023-09-13 15:58:53 +0900

    [Silver I] Title: 회전 초밥, Time: 204 ms, Memory: 18376 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main { 
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static int N, D, K, C;
+    public static int[] A;
+    
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        A = new int[N+K-1];
+        for(int i = 0; i < N; i++)
+            A[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < K-1; i++)
+            A[N+i] = A[i];
+        System.out.println(solve());
+    }   
+    
+    public static int solve(){
+        int ret = 0;
+        int dishes = 0;
+        int speciesCnt = 0;
+        int l = 0, r = 0;
+        int[] has = new int[D+1];
+        
+        while(r < N + K - 1){
+            if(dishes > K){
+                has[A[l]]--;
+                if(has[A[l]] == 0) speciesCnt--;
+                dishes--;
+                l++;
+            }
+            else{
+                if(has[A[r]] == 0) speciesCnt++;
+                has[A[r]]++;
+                dishes++;
+                r++;
+            }
+            if(dishes <= K){
+                ret = Math.max(ret, has[C] == 0? speciesCnt + 1 : speciesCnt);
+            }
+        }
+        
+        while(l < r && dishes > K){
+            has[A[l]]--;
+            if(has[A[l]] == 0) speciesCnt--;
+            dishes--;
+            l++;
+            if(dishes <= K) ret = Math.max(ret, has[C] == 0? speciesCnt + 1 : speciesCnt);
+        }
+        return ret;
+    }
+}
