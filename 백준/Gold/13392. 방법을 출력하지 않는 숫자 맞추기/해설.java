41bb9719a0cabcbd46ee8484bcfab276a8cfedcf
pug <141905308+kiki9484@users.noreply.github.com>
2023-10-14 16:01:08 +0900

    Update and rename 방법을 출력하지 않는 숫자 맞추기.java to 해설.java

--- 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/해설.java
+++ 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/해설.java
@@ -1,3 +1,21 @@
+/*
+문제1. 방법을 출력하지 않는 숫자 맞추기
+
+조건1. 왼쪽으로 돌리면 아래에 있는 나사가 같이 돈다.
+조건2. 오른쪽으로 돌리면 하나만 돌아간다.
+
+D[i][j] = i번 나사가 현재 왼쪽으로 j번 돌은 상황일 때 최소 횟수
+
+1) 왼쪽으로 돌리는 경우
+int costL = (b[index] - cur + 10) % 10;
+// 아래쪽 나사가 돌아가야 하는 수를 두번째 매개변수에 저장
+int left = go(index+1, (turn + costL) % 10) + costL; 
+
+2) 오른쪽으로 돌리는 경우
+int costR = (cur - b[index] + 10) % 10;
+int right = go(index+1, turn) = costR;
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
