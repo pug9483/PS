0a9f73e90db7f2ead4fa2d31db2c183b29fcee33
pug <141905308+kiki9484@users.noreply.github.com>
2023-12-30 16:38:16 +0900

    [Silver II] Title: 한 줄로 서기, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -5,41 +5,17 @@ public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static int N;
     public static int[] A;
-    public static boolean[] visited;
-    public static int[] arr;
+    public static List<Integer> ranks = new ArrayList<>();
     
     public static void main(String[] args) throws IOException{
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        A = new int[N];
-        arr = new int[N];
-        visited = new boolean[N];
-        
+        A = new int[N+1];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++)
+        for(int i = 1; i <= N; i++)
             A[i] = Integer.parseInt(st.nextToken());
-        
-        solve(0);
-    }
-    
-    public static boolean solve(int here){
-        if(here == N){
-            for(int i = 0; i < N; i++){
-                int cnt = 0;
-                for(int j = 0; j < i; j++)
-                    if(arr[i] < arr[j]) cnt++;
-                if(cnt != A[arr[i]-1]) return false;
-            }
-            for(int i = 0; i < N; i++) System.out.print(arr[i] + " ");
-            return true;
-        }
-        for(int i = 0; i < N; i++){
-            if(visited[i]) continue;
-            arr[here] = i+1;
-            visited[i] = true;
-            if(solve(here+1)) return true;
-            visited[i] = false;
-        }
-        return false;
+        for(int i = N; i > 0; i--)
+            ranks.add(A[i], i);
+        ranks.stream().forEach(s -> System.out.print(s + " "));
     }
 }
