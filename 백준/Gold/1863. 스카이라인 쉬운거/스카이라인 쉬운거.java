d2700f23e6d0432c8dd4dbac175928d12f0a9bcd
pug <141905308+kiki9484@users.noreply.github.com>
2024-01-01 16:56:32 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 216 ms, Memory: 19528 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -2,59 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main { 
-    static class Node{
-        int x;
-        int y;
-        public Node(int x, int y){
-            this.x = x;
-            this.y = y;
-            
-        }
-    }
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static int N;
-    public static int[][] A;
+    public static int[] A;
     
     public static void main(String[] args) throws IOException{
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        A = new int[N][2];
-        int maxValue = 0;
+        A = new int[N];
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            A[i][0] = x;
-            A[i][1] = y;
-            maxValue = Math.max(maxValue, A[i][1]);
+            A[i] = y;
         }
-        System.out.println(maxValue == 0? 0 : solve());
+        System.out.println(solve());
     }
     
    public static int solve(){
        int ret = 0;
-       Stack<Node> stack = new Stack<>();
+       Stack<Integer> stack = new Stack<>();
+       
        for(int i = 0; i < N; i++){
-           int x = A[i][0];
-           int y = A[i][1];
            if(stack.isEmpty()){
-               stack.add(new Node(x, y));  
+               if(A[i] > 0)stack.add(A[i]);  
                continue;
            } 
-           Node prev = stack.peek();
-           if(prev.y < y) stack.add(new Node(x, y));
-           else{
-               while(!stack.isEmpty()){
-                   Node node = stack.pop();
-                   if(node.y == 0) continue;
-                   if(node.y < y) {
-                       stack.add(node);
-                       break;
-                   }
-                   if(node.y > y) ret++;
+           while(!stack.isEmpty()){
+               int py = stack.pop();
+               if(py > A[i]) ret++;
+               else if(py < A[i]){
+                   stack.add(py);
+                   break;
                }
-               stack.add(new Node(x, y));
            }
+           if(A[i] > 0) stack.add(A[i]);
        }
        ret += stack.size();
        return ret;
