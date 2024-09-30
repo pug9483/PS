39d5924992417b5ef39015d0f9d1752df3c75e40
parkuigyun <67894738+pug9483@users.noreply.github.com>
2024-09-30 16:14:35 +0900

    [Gold III] Title: 색상환, Time: 108 ms, Memory: 18204 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.java
+++ 백준/Gold/2482. 색상환/색상환.java
@@ -1,45 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main { 
+public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static int N, K;
-    public static long MOD = 1_000_000_003L;
-    public static long[][][] dp;
-    
-    public static void main(String[] args) throws IOException{
+    public static int N;
+    public static int K;
+    public static final int MOD = 1_000_000_003;
+
+    public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         K = Integer.parseInt(br.readLine());
-        dp = new long[3][N+1][N+1];
-        for(int i = 0; i < 3; i++)
-            for(int j = 0; j < N+1; j++)
-                Arrays.fill(dp[i][j], -1L);
-        long ret = solve(1, 2, 1); // 1 : 0번 색깔 선택, 2: 0번에 색깔 x
-        ret = (ret + solve(2, 1, 0)) % MOD;
-        System.out.println(ret);
+        System.out.println(solve());
     }
-    
-    public static long solve(int first, int index, int cnt){
-        if(index >= N)  return cnt == K? 1L : 0L;        
-        
-        if(dp[first][index][cnt] != -1L) return dp[first][index][cnt];
-        long ret = 0L;
-        
-        if(index < N - 1){
-            // index번을 선택하는 경우
-            ret = (ret + solve(first, index+2, cnt+1)) % MOD;
-            // index번을 선택하지 않는 경우
-            ret = (ret + solve(first, index+1, cnt)) % MOD;
+
+    public static int solve() {
+        int ret = 0;
+        int[][] dp = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            dp[i][1] = i;
+            dp[i][0] = 1;
         }
-        else if(index == N - 1){
-            if(first == 1L) ret = (ret + solve(first, index+1, cnt)) % MOD;
-            else{
-                ret = (ret + solve(first, index+2, cnt+1)) % MOD;
-                ret = (ret + solve(first, index+1, cnt)) % MOD;
+
+        for (int i = 3; i <= N; i++) {
+            for (int j = 1; j <= (i + 1) / 2; j++) {
+                dp[i][j] = (dp[i - 2][j - 1] + dp[i - 1][j]) % MOD;
             }
         }
-        return dp[first][index][cnt] = ret;
+        ret += (dp[N - 3][K - 1] + dp[N - 1][K]) % MOD;
+
+
+        return ret;
     }
 }
