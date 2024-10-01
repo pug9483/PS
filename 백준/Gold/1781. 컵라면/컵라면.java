4de9f30830cba920b897b52170ae14b4011ecd79
parkuigyun <67894738+pug9483@users.noreply.github.com>
2024-10-01 17:45:43 +0900

    [Gold II] Title: 컵라면, Time: 724 ms, Memory: 72512 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -1,60 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Pair{
-        int deadLine;
+    static class Node{
+        int deadline;
         int count;
-        
-        public Pair(int deadLine, int count){
-            this.deadLine = deadLine;
+
+        public Node(int deadline, int count) {
+            this.deadline = deadline;
             this.count = count;
         }
     }
-    
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static int N;
-    public static List<Pair> list = new ArrayList<>();
-    
-    public static void main(String args[]) throws IOException {
+    public static List<Node> list = new ArrayList<>();
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int deadLine = Integer.parseInt(st.nextToken());
+            int deadline = Integer.parseInt(st.nextToken());
             int count = Integer.parseInt(st.nextToken());
-            list.add(new Pair(deadLine, count));
+            list.add(new Node(deadline, count));
         }
-        
+        System.out.println(solve());
+    }
+
+    public static int solve() {
         int ret = 0;
-        
-        list.sort((o1, o2) -> {
-            int cmp = Integer.compare(o1.deadLine, o2.deadLine);
-            return cmp == 0 ? Integer.compare(o2.count, o1.count) : cmp; 
-        });
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1, o2));
-        
-        for(Pair pair : list){
-            int deadLine = pair.deadLine;
-            int count = pair.count;
-            // pq의 size가 0이면 추가하기
-            if(pq.isEmpty() || pq.size() < deadLine){
-                pq.add(count);
-            }
-            // pq의 첫번째 값보다 count의 값이 작으면 넘어가기
-            else if(pq.peek() > count) continue;
-            // pq의 첫번째 값보다 count의 값이 크면 교체하기
-            else if(pq.peek() < count){
+        list.sort((Comparator.comparingInt(o -> o.deadline)));
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.count));
+        pq.add(list.get(0));
+
+        for (int i = 1; i < N; i++) {
+            Node here = list.get(i);
+            Node lowest = pq.peek();
+            if(pq.size() < here.deadline) pq.add(here);
+            else if(lowest.count < here.count){
                 pq.poll();
-                pq.add(count);
+                pq.add(here);
             }
         }
-        
-        while(!pq.isEmpty()) ret += pq.poll();
-            
-        System.out.println(ret);
+
+        return pq.stream().mapToInt(o -> o.count).sum();
     }
 }
