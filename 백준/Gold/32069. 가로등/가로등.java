4e30b7786f2c1d343ae3884d376b2774ad60892a
pug <67894738+pug9483@users.noreply.github.com>
2024-11-14 14:59:33 +0900

    [Gold V] Title: 가로등, Time: 2660 ms, Memory: 466568 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.java
+++ 백준/Gold/32069. 가로등/가로등.java
@@ -1,42 +1,43 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     static class Node{
-        int num;
-        int bright;
+        BigInteger num;
+        BigInteger bright;
 
-        public Node(int num, int bright) {
+        public Node(BigInteger num, BigInteger bright) {
             this.num = num;
             this.bright = bright;
         }
     }
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static Long L;
+    static long L;
     static int N, K;
-    static int[] A;
-    static int[] next = new int[]{-1, 1};
+    static BigInteger[] A;
+    static BigInteger[] next = new BigInteger[]{BigInteger.ONE.multiply(BigInteger.valueOf(-1)), BigInteger.ONE};
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         L = Long.parseLong(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        A = new int[N];
+        A = new BigInteger[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            A[i] = new BigInteger(st.nextToken());
         }
         solve();
     }
 
     private static void solve() {
         Queue<Node> q = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
+        Set<BigInteger> visited = new HashSet<>();
 
         for (int i = 0; i < N; i++) {
-            q.add(new Node(A[i], 0));
+            q.add(new Node(A[i], BigInteger.ZERO));
             visited.add(A[i]);
         }
 
@@ -48,10 +49,10 @@ public class Main {
             K--;
             if(K == 0) break;
             for (int i = 0; i < 2; i++) {
-                int nextNum = here.num + next[i];
-                if(visited.contains(nextNum) || nextNum < 0 || nextNum > L) continue;
+                BigInteger nextNum = here.num.add(next[i]);
+                if(visited.contains(nextNum) || nextNum.compareTo(BigInteger.valueOf(0)) < 0 || nextNum.compareTo(BigInteger.valueOf(L)) > 0) continue;
                 visited.add(nextNum);
-                q.add(new Node(nextNum, here.bright + 1));
+                q.add(new Node(nextNum, here.bright.add(BigInteger.valueOf(1))));
             }
         }
         System.out.print(sb);
