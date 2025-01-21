4ab58ff1bd385a257cccb8480c425b91084cf4e9
pug <67894738+pug9483@users.noreply.github.com>
2025-01-21 22:15:54 +0900

    [Gold IV] Title: 편세권, Time: 708 ms, Memory: 213140 KB -BaekjoonHub

--- 백준/Gold/31849. 편세권/편세권.java
+++ 백준/Gold/31849. 편세권/편세권.java
@@ -2,67 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static List<int[]> rooms = new ArrayList<>();
+    static Queue<int[]> q = new LinkedList<>();
+    static int[][] dist;
+    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {0, 0, 1, -1};
+    static int N;
+    static int M;
+    static int R;
+    static int C;
+    static int INF = 987654321;
 
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-		ArrayList<int[]> rooms = new ArrayList<>();
-		for (int i = 0; i < R; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken()) - 1;
-			int y = Integer.parseInt(st.nextToken()) - 1;
-			int c = Integer.parseInt(st.nextToken());
-			rooms.add(new int[] {x, y, c});
-		}
+        for (int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine());
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+            rooms.add(new int[]{y, x, w});
+        }
 
-		int[][] dist = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				dist[i][j] = Integer.MAX_VALUE;
-			}
-		}
+        dist = new int[N][M];
+        for(int i = 0; i < N; i++)
+            Arrays.fill(dist[i], INF);
 
-		Queue<int[]> queue = new ArrayDeque<>();
-		for (int i = 0; i < C; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken()) - 1;
-			int y = Integer.parseInt(st.nextToken()) - 1;
-			dist[x][y] = 0;
-			queue.add(new int[] {x, y, 0});
-		}
+        for (int i = 0; i < C; i++) {
+            st = new StringTokenizer(br.readLine());
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            q.add(new int[]{y, x});
+            dist[y][x] = 0;
+        }
+        System.out.println(solve());
+    }
 
-		int[] dx = {1, -1, 0, 0};
-		int[] dy = {0, 0, 1, -1};
-		while (!queue.isEmpty()) {
-			int[] curr = queue.poll();
-			int x = curr[0];
-			int y = curr[1];
-			int d = curr[2];
-			for (int k = 0; k < 4; k++) {
-				int tx = x + dx[k];
-				int ty = y + dy[k];
-				if (tx < 0 || ty < 0 || tx >= N || ty >= M) {
-					continue;
-				}
-				if (dist[tx][ty] <= d + 1) {
-					continue;
-				}
-				dist[tx][ty] = d + 1;
-				queue.add(new int[] {tx, ty, d + 1});
-			}
-		}
+    private static int solve() {
+        while (!q.isEmpty()) {
+            int[] here = q.poll();
+            for (int dir = 0; dir < 4; dir++) {
+                int ny = here[0] + dy[dir];
+                int nx = here[1] + dx[dir];
+                if (ny < 0 || ny >= N || nx <0 || nx >= M || dist[ny][nx] != INF) continue;
+                dist[ny][nx] = dist[here[0]][here[1]] + 1;
+                q.add(new int[] {ny, nx});
+            }
+        }
 
-		int answer = Integer.MAX_VALUE;
-		for (int[] room : rooms) {
-			answer = Math.min(answer, room[2] * dist[room[0]][room[1]]);
-		}
-		System.out.println(answer);
-	}
+        int ret = INF;
+        for (int[] room : rooms) {
+            ret = Math.min(ret, room[2] * dist[room[0]][room[1]]);
+        }
+        return ret;
+    }
 }
