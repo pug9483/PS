a009fa664acc617854ba67b0645343ed38f1006e
pug <67894738+pug9483@users.noreply.github.com>
2025-02-18 13:04:47 +0900

    [Gold IV] Title: 키 순서, Time: 1140 ms, Memory: 170420 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -4,49 +4,35 @@ class Main
 {
 	static int N;
 	static int M;
-	static List<List<Integer>> graph = new ArrayList<>();
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
 		
-		boolean[][] visited = new boolean[N][N];
-		for(int i = 0; i < N; i++) graph.add(new ArrayList<>());
+		int[][] dist = new int[N][N];
 		for(int i = 0; i < M; i++) {
 			int u = sc.nextInt()- 1;
 			int v = sc.nextInt()- 1;
-			graph.get(u).add(v);
-			
+			dist[u][v] = 1;
 		}
-		for(int i = 0; i < N; i++) {
-			solve(i, visited);
+		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					dist[i][j] |= (dist[i][k] & dist[k][j]);
+				}
+			}
 		}
 		
 		int ret = 0;
 		for(int i = 0; i < N; i++) {
 			int sum = 0;
 			for(int j = 0; j < N; j++) {
-				if(visited[i][j] || visited[j][i]) sum++;
+				if(dist[i][j] == 1 || dist[j][i] == 1) sum++;
 			}
 			if(sum == N-1) ret++;
 		}
 		System.out.println(ret);
 	}
-	
-	static void solve(int here, boolean[][] visited) {
-		for(int next: graph.get(here)) {
-			if(!visited[here][next]) {
-				visited[here][next] = true;
-				solve(next, visited);
-			}
-			for(int j = 0; j < N; j++) {
-				if(visited[next][j]) {
-					visited[here][j] = true;
-				}
-			}
-		}
-	}
-	
 }
-
