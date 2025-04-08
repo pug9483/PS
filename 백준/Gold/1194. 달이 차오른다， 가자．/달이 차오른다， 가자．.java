e835de0b3b052424d289015d59bf63763ce766bf
pug <67894738+pug9483@users.noreply.github.com>
2025-04-08 14:36:39 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 132 ms, Memory: 17532 KB -BaekjoonHub

---
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Node{
+		int y;
+		int x;
+		int set;
+
+		public Node(int y, int x, int set) {
+			this.y = y;
+			this.x = x;
+			this.set = set;
+		}
+	}
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static int M;
+	static char[][] board;
+	static final int INF = 987654321;
+	static final int[] dy = {1, -1, 0, 0};
+	static final int[] dx = {0, 0, -1, 1};
+
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		board = new char[N][M];
+		for (int i = 0; i < N; i++) {
+			board[i] = br.readLine().toCharArray();
+		}
+		System.out.println(solve());
+	}
+
+	public static int solve() {
+		Queue<Node> q = new LinkedList<>();
+		int[][][] dist = new int[N][M][2<<6];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				Arrays.fill(dist[i][j], INF);
+			}
+		}
+
+		out:
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if(board[i][j] == '0'){
+					q.add(new Node(i, j, 0));
+					dist[i][j][0] = 0;
+					break out;
+				}
+			}
+		}
+
+		while (!q.isEmpty()) {
+			Node here = q.poll();
+			int d = dist[here.y][here.x][here.set];
+			if(board[here.y][here.x] == '1') return d;
+			for (int dir = 0; dir < 4; dir++) {
+				int ny = here.y + dy[dir];
+				int nx = here.x + dx[dir];
+				if(ny < 0 || ny >= N || nx < 0 || nx >= M || board[ny][nx] == '#') continue;
+
+				if (board[ny][nx] == 'a' || board[ny][nx] == 'b' || board[ny][nx] == 'c' || board[ny][nx] == 'd'
+					|| board[ny][nx] == 'e' || board[ny][nx] == 'f') {
+					int newSet = here.set | (1 << (board[ny][nx] - 'a'));
+					if(dist[ny][nx][newSet] != INF) continue;
+					dist[ny][nx][newSet] = d + 1;
+					q.add(new Node(ny, nx, newSet));
+				}
+				else if (board[ny][nx] == 'A' || board[ny][nx] == 'B' || board[ny][nx] == 'C' || board[ny][nx] == 'D'
+					|| board[ny][nx] == 'E' || board[ny][nx] == 'F') {
+					if ((here.set & 1 << (board[ny][nx] - 'A')) == 0) continue;
+					if(dist[ny][nx][here.set] != INF) continue;
+					dist[ny][nx][here.set] = d + 1;
+					q.add(new Node(ny, nx, here.set));
+
+				}
+				else if(dist[ny][nx][here.set] == INF){
+					dist[ny][nx][here.set] = d + 1;
+					q.add(new Node(ny, nx, here.set));
+				}
+			}
+		}
+
+		return -1;
+	}
+}
