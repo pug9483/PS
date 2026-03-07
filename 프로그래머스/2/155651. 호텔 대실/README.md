6ba79000f4206e9de24beb12f11fb937cf4155c1
pug <67894738+pug9483@users.noreply.github.com>
2026-03-07 20:09:09 +0900

    [level 2] Title: 호텔 대실, Time: 5.21 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=java) 
 
 ### 성능 요약
 
-메모리: 76.1 MB, 시간: 9.54 ms
+메모리: 81.4 MB, 시간: 5.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 10:14:13
+2026년 03월 07일 20:09:05
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,44 +1,32 @@
-import java.util.*;
 class Solution {
-    static class Time {
-        int start;
-        int end;
-        
-        public Time(int start, int end){
-            this.start = start;
-            this.end = end;
-        }
-    }
+    public int SIZE = 1450;
     
-    int N;
-    List<Time> list = new ArrayList<>();
-    public int solution(String[][] bookTime) {
-        int ret = 0;
-        N = bookTime.length;
-        
-        for(int i = 0; i < N; i++){
-            String[] time = bookTime[i];
-            String[] start = time[0].split(":");
-            String[] end = time[1].split(":");
-            int startTime = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
-            int endTime = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
-            
-            list.add(new Time(startTime, endTime));
+    public int solution(String[][] book_time) {
+        int[] rooms = new int[SIZE]; 
+
+        for (String[] time : book_time) {
+            int start = parseTime(time[0]);
+            int end = parseTime(time[1]) + 10; 
+
+            rooms[start] += 1; 
+            rooms[end] -= 1;   
         }
-        
-        list.sort((o1, o2) -> Integer.compare(o1.start, o2.start));
-        
-        PriorityQueue<Time> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.end, o2.end));
-        for(Time time: list) {
-            if(!pq.isEmpty()){
-                Time first = pq.peek();
-                if(first.end + 10 <= time.start){
-                    pq.poll();
-                }    
-            }
-            pq.add(time);
-            ret = Math.max(ret, pq.size());
+
+        int answer = 0;
+        int currentRooms = 0;
+
+        for (int i = 0; i < SIZE; i++) {
+            currentRooms += rooms[i];
+            answer = Math.max(currentRooms, answer);
         }
-        return ret;
+
+        return answer;
+    }
+
+    private int parseTime(String time) {
+        String[] split = time.split(":");
+        int hour = Integer.parseInt(split[0]);
+        int minute = Integer.parseInt(split[1]);
+        return hour * 60 + minute;
     }
 }
