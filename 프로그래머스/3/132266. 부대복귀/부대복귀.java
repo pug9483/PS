0584addb1b3e761e46f1491b7238e6b22454066a
pug <67894738+pug9483@users.noreply.github.com>
2026-03-22 13:11:31 +0900

    [level 3] Title: 부대복귀, Time: 168.08 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,44 +1,41 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>> graph = new ArrayList<>();
-    int N;
+    private List<List<Integer>> graph;
+    
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        N = n;
-        for(int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        graph = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
         
         for(int i = 0; i < roads.length; i++){
-            int u = roads[i][0];
-            int v = roads[i][1];
+            int u = roads[i][0] - 1;
+            int v = roads[i][1] - 1;
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
         
-        int[] dist = bfs(destination);
-        
-        int[] answer = new int[sources.length];
-        for(int i = 0; i < sources.length; i++){
-            answer[i] = dist[sources[i]];
-        }
-        return answer;
-    }
-    
-    public int[] bfs(int src){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(src);
-        
-        int[] dist = new int[N+1];
+        int[] dist = new int[n];
         Arrays.fill(dist, -1);
-        dist[src] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(destination - 1);
+        dist[destination - 1] = 0;
         
         while(!q.isEmpty()){
             int here = q.poll();
-            for(int next: graph.get(here)){
-                if(dist[next] >= 0) continue;
-                q.add(next);
-                dist[next] = dist[here] + 1;
+            for(int there: graph.get(here)){
+                if(dist[there] == -1){
+                    dist[there] = dist[here] + 1;
+                    q.add(there);
+                }
             }
         }
-        return dist;
+        
+        int[] answer = new int[sources.length];
+        for(int i = 0; i < sources.length; i++){
+            answer[i] = dist[sources[i] - 1];
+        }
+        return answer;
     }
 }
